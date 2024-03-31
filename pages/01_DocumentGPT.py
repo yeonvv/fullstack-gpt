dcb16b13cef09042eb34195a2ac816c71f887707
yeonvv <yeonvv8@gmail.com>
2024-03-31 17:50:41 +0900

    add loadfile logic and add ignore .streamlit/

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
-from time import sleep
+from langchain.storage import LocalFileStore
+from langchain.embeddings import OpenAIEmbeddings, CacheBackedEmbeddings
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import FAISS
 
 st.set_page_config(
     page_title="DocumentGPT",
@@ -9,27 +13,46 @@ st.set_page_config(
 
 st.title("DocumentGPT")
 
-if "messages" not in st.session_state:
-    st.session_state["messages"] = []
+st.markdown(
+    """
+Welcome!
+
+Use this chatbot to ask question to an AI about your files!
+"""
+)
 
 
-def send_message(message, role, save=True):
-    with st.chat_message(name=role):
-        st.write(message)
-    if save:
-        st.session_state["messages"].append({"message": message, "role": role})
-
-
-for message in st.session_state["messages"]:
-    send_message(
-        message=message["message"],
-        role=message["role"],
-        save=False,
+def embed_file(file):
+    file_content = file.read()
+    file_path = f"./.cache/files/{file.name}"
+    with open(file=file_path, mode="wb") as f:
+        f.write(file_content)
+    embeddings = OpenAIEmbeddings()
+    loader = UnstructuredFileLoader(
+        f"./.cache/files/{file.name}",
     )
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100,
+    )
+    docs = loader.load_and_split(text_splitter=splitter)
+    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
+    cache_embeddings = CacheBackedEmbeddings.from_bytes_store(
+        embeddings,
+        cache_dir,
+    )
+    vector_store = FAISS.from_documents(docs, cache_embeddings)
+    retriever = vector_store.as_retriever()
+    return retriever
 
-message = st.chat_input(placeholder="To ask for AI")
 
-if message:
-    send_message(message=message, role="human")
-    sleep(1)
-    send_message(f"You said {message}", role="ai")
+file = st.file_uploader(
+    label="Upload a .txt .pdf or .docx file",
+    type=["pdf", "txt", "docx"],
+)
+
+question = st.chat_input(placeholder="Ask your question in document")
+
+if file:
+    retriever = embed_file(file)
